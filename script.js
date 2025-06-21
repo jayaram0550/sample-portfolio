50ec96eb80c1745dba2e5e6513517b20793f0e96
Palla Naga Sai Jayaram <150007395+jayaram0550@users.noreply.github.com>
2025-06-21 15:37:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ backSpeed:100,
 backDelay:1000, 
 loop:true 
 }); 
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+};
